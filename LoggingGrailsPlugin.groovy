4198cefadf81afdf600ac59dbe81c9286b2c0813
Richard Vowles <richard@bluetrainsoftware.com>
2013-02-21 20:17:46 +1300

    remove resources

--- LoggingGrailsPlugin.groovy
+++ LoggingGrailsPlugin.groovy
@@ -2,7 +2,7 @@
 
 
 class LoggingGrailsPlugin {
-  def version = "2.2"
+  def version = "2.4"
   def grailsVersion = "2.0.1 > *"
   def dependsOn = [:]
 
@@ -15,6 +15,7 @@ class LoggingGrailsPlugin {
 
   def pluginExcludes = [
           'grails-app/controllers/**',
+          'grails-app/conf/spring/resources.*', // the dreaded resources.xml wipeout
           'grails-app/i18n/**',
           'web-app/**'
   ]
